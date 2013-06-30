af75b65a4793c4169be13b126fc53d4d6a87354b
Victor Tkachenko <tkachenkodp040@ukr.net>
2013-06-30 03:32:58 +0300

    Gadget v1 Non-refactor

--- timers/timer.js
+++ timers/timer.js
@@ -3,9 +3,31 @@ window.onload = function ()
 	var clocks = setInterval(times,1000),
 		flagLeft = true,
 		flagRight = true,		
-		clock = document.getElementById("clock");
-	
+		clock = document.getElementById("clock"),
+		draggable = null;
+		
 	clock.innerHTML = "This Date";
+	
+	function capture()
+	{
+		if(!draggable)
+		{
+			draggable = this;
+		}
+	}	
+	function release()
+	{
+		draggable = null;
+	}
+	function move(event)
+	{
+		if(draggable)
+		{
+			draggable.style.left = event.pageX - 25 + 'px';
+			draggable.style.top = event.pageY - 25 + 'px';
+		}
+	}
+	
 	
 	function fixFormat(number)
 	{
@@ -30,6 +52,10 @@ window.onload = function ()
 		{
 			clock.innerHTML = fixFormat(hour)+":"+fixFormat(minutes)+":"+fixFormat(sec);
 		}
+		if(flagRight)
+		{
+			clock.innerHTML = fixFormat(day)+":"+fixFormat(month)+":"+fixFormat(year);
+		}
 	}
 	
 	clock.onclick = function ()
@@ -42,4 +68,7 @@ window.onload = function ()
 		flagRight = flagRight ? (flagRight = false) : (flagRight = true);
 		return false;
 	}
+	clock.onmousedown = capture;
+	clock.onmouseup = release;
+	window.onmousemove = move;
 }
